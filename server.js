6d8a882608a472f3030e874016b81f548993b35c
AlCast <alvaro.castelan.21@gmail.com>
2018-05-23 23:05:19 -0500

    added html routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 var app = express();
 
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json()); 
 
 // routes to files 
-
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT)
